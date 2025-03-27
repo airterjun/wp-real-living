ebb67b7bd565415a9797902cbf179074871d9d0d
Macbook Pro <move.thinkcreative@gmail.com>
2025-03-28 07:15:20 +0800

    Update remove no scalable

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0">
     <meta name="format-detection" content="telephone=no">
     <meta name="theme-color">
 
