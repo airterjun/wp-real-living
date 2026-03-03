0b24b28eba2664ce0aed3dcc0a6c450232fa2901
airterjun <move.thinkcreative@gmail.com>
2026-03-04 05:09:47 +0800

    Update header

--- header.php
+++ header.php
@@ -18,7 +18,38 @@
 
     <title><?php the_title() ?></title>
 
-    <link rel="stylesheet" href="https://use.typekit.net/pkj7fwh.css" crossorigin>
+    <!-- <link rel="stylesheet" href="https://use.typekit.net/pkj7fwh.css" crossorigin> -->
+
+    <script>
+        (function(d) {
+            var config = {
+                    kitId: 'pkj7fwh',
+                    scriptTimeout: 3000,
+                    async: true
+                },
+                h = d.documentElement,
+                t = setTimeout(function() {
+                    h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+                }, config.scriptTimeout),
+                tk = d.createElement("script"),
+                f = false,
+                s = d.getElementsByTagName("script")[0],
+                a;
+            h.className += " wf-loading";
+            tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+            tk.async = true;
+            tk.onload = tk.onreadystatechange = function() {
+                a = this.readyState;
+                if (f || a && a != "complete" && a != "loaded") return;
+                f = true;
+                clearTimeout(t);
+                try {
+                    Typekit.load(config)
+                } catch (e) {}
+            };
+            s.parentNode.insertBefore(tk, s)
+        })(document);
+    </script>
 
     <?php
     $custom_class = get_post_meta(get_the_ID(), 'page_class_element', true);
