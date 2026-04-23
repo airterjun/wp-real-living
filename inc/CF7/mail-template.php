0edbaa16073d2fe49853ee28ad8fea4a9a014d37
airterjun <move.thinkcreative@gmail.com>
2026-04-23 22:25:33 +0800

    Update mail templating

--- inc/CF7/mail-template.php
+++ inc/CF7/mail-template.php
@@ -407,7 +407,7 @@ add_action('admin_init', function () {
                                 'ticket_id' => cf7cmt_generate_ticket_id()
                         ]);
 
-                        $status = $sent ? 'success' : 'error';
+                        $status = $sent ? 'success' . $test_email : 'error' . $test_email;
                 }
 
                 // 🔥 REDIRECT DENGAN QUERY
